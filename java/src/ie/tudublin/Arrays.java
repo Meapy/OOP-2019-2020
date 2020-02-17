a630b7714436c8bf7ccc4368ab2a7fc86fb29948
Daniel Krasovski <dkrasovskijj@gmail.com>
2020-02-17 12:56:32 +0000

    arrays

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -3,17 +3,45 @@ package ie.tudublin;
 import processing.core.PApplet;
 
 public class Arrays extends PApplet
-{	
+{
+
+	float[] rainfall = {45, 37, 55, 27, 38, 50, 79, 48, 104, 31, 100, 50};
+	String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "OCct" ,"Nov", "Dec"};
 	public void settings()
 	{
-		size(500, 500);
+		size(1000, 500);
 	}
 
 	public void setup() 
 	{
-		
+		float min = rainfall[0];
+		float max = rainfall[0];
+
+		String MinM = months[0];
+		String MaxM = months[0];
+
+		for(int i = 0; i < rainfall.length; i++){
+			if(rainfall[i] < min) {
+				min = rainfall[i];
+				MinM = months[i];
+			}
+			if(rainfall[i] > max)
+			{
+				max = rainfall[i];
+				MaxM = months[i];
+			}
+		}
+		println( MinM+ " " + min + " " +MaxM + " " +  max);
 	}
 
+	public void drawBarChart(){
+		int j = 0;
+		for(int i = 0; i < rainfall.length; i++){
+			fill(10 + (i * 10),100,100+(i * 10));
+			j += 70;
+			rect(j,  500 , -50, rainfall[i] * -1);
+		}
+	}
 	
 
 	float offset = 0;
@@ -31,7 +59,14 @@ public class Arrays extends PApplet
 
 	public void draw()
 	{	
-		background(0);		
-		colorMode(HSB);	
+		background(50);
+		colorMode(HSB);
+
+		drawBarChart();
+	}
+
+	public static void main(String... args) {
+		PApplet.main("ie.tudublin.Arrays");
+
 	}
 }
